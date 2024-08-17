c513ca8f8f3648db93e584dd56f1559369b9bc08
Sobuz391 <monowerhossaisobuz@gmail.com>
2024-08-17 19:15:33 +0600

    connected to the mongodb database store

--- server.js
+++ server.js
@@ -8,3 +8,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+    throw new Error("MONGODB_URI is not defined in environment variables.");
+}
