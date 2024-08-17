f521e6baec254cc84645d5542a540d4a36a1e9a7
Sobuz391 <monowerhossaisobuz@gmail.com>
2024-08-17 19:17:10 +0600

    server is running and send the api started succesfully

--- server.js
+++ server.js
@@ -16,3 +16,28 @@ if (!uri) {
 
 let productCollection;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        productCollection = client.db("ecommerce").collection("products");
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Failed to connect to MongoDB:', error);
+        process.exit(1);
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+
+async function startServer() {
+    await connectToDatabase();
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
+
+startServer();
